a9bc2ab1b97a673524821b93af8c9870d97b82eb
Gustavo Maia <146839333+gustavoGMM@users.noreply.github.com>
2024-04-06 11:24:33 -0300

    update

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,3 +1,27 @@
+document.getElementById('showPasswordBtn').addEventListener('click', function() {
+    togglePasswordVisibility(true);
+  });
+  
+  document.getElementById('hidePasswordBtn').addEventListener('click', function() {
+    togglePasswordVisibility(false);
+  });
+  
+  function togglePasswordVisibility(show) {
+    const senhaDoUsuario = document.getElementById('senhaDoUsuario');
+    const showPasswordBtn = document.getElementById('showPasswordBtn');
+    const hidePasswordBtn = document.getElementById('hidePasswordBtn');
+  
+    if (show) {
+        senhaDoUsuario.type = 'text';
+        showPasswordBtn.style.display = 'none';
+        hidePasswordBtn.style.display = 'block';
+    } else {
+        senhaDoUsuario.type = 'password';
+        showPasswordBtn.style.display = 'block';
+        hidePasswordBtn.style.display = 'none';
+    }
+  }
+
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita o envio do formulário padrão
   
