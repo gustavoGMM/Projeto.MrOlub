5b6d4b9aef2d1bc064cf90c1eeeb114aa01f501f
Gustavo Maia <146839333+gustavoGMM@users.noreply.github.com>
2024-04-08 18:56:03 -0300

    update

--- src/auth/login.js
+++ src/auth/login.js
@@ -65,3 +65,47 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     elemento.style.display = 'block';
   }
   
+
+  // Evita o envio do formulário padrão
+
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const nomeDeUsuario = document.getElementById('nomeDeUsuario').value;
+    const senhaDoUsuario = document.getElementById('senhaDoUsuario').value;
+  
+    fetch('http://localhost:8080/api/auth/user/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nomeDeUsuario: nomeDeUsuario,
+            senhaDoUsuario: senhaDoUsuario
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            exibirMensagem('mensagemErro', 'Erro ao realizar o login. Por favor, verifique suas credenciais e tente novamente.');
+            throw new Error('Erro ao realizar o login.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.role === "FUNCIONARIO_ADMINISTRACAO") {
+            sessionStorage.setItem('userToken', data.token);
+            window.location.href = "/pages/pagAdm/index.html"; 
+        } else {
+            exibirMensagem('mensagemErro', 'Usuário não tem autorização.');
+        }
+    })
+    .catch(error => {
+        console.error('Erro ao tentar fazer o login:', error);
+    });
+});
+
+function exibirMensagem(idElemento, mensagem) {
+    const elemento = document.getElementById(idElemento);
+    elemento.textContent = mensagem;
+    elemento.style.display = 'block';
+}
