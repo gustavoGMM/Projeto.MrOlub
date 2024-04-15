5e54ef57e9f535fa35f6e2bcbe6b45cc9502869f
Gustavo Maia <146839333+gustavoGMM@users.noreply.github.com>
2024-04-15 16:31:48 -0300

    melhoria no botao de sair agora vai pra tela de login  (OBS  apenas redireciona)

--- pages/pagAdm/logout.php
+++ pages/pagAdm/logout.php
@@ -1,19 +1,11 @@
 <?php
-// Inicia a sessão (se já não estiver iniciada)
+// Inicia a sessão
 session_start();
 
 // Destroi todas as variáveis de sessão
-$_SESSION = [];
-
-// Invalida o cookie de sessão
-if (isset($_COOKIE[session_name()])) {
-    setcookie(session_name(), '', time() - 86400, '/');
-}
-
-// Finaliza a sessão
 session_destroy();
 
-// Redireciona o usuário de volta para a página de login (substitua "login.php" pelo caminho correto)
-header("Location: login.php");
+// Redireciona para a página de login
+echo "<script>window.location.href = '/pages/Loguin/loguin.html';</script>";
 exit;
 ?>
